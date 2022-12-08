a5eedae6827d5a45270f8857fffa3ce7b0d77ab4
James Purvis <110248591+JamesPurvis@users.noreply.github.com>
2022-12-08 13:35:52 -0700

    refactor navigator
    refactor navigator

--- Emulator/Emulator/Environment.cs
+++ Emulator/Emulator/Environment.cs
@@ -3,6 +3,7 @@ using Emulator.Network;
 using Emulator.Messages;
 using Emulator.Game.Database;
 using Emulator.Network.Session;
+using Emulator.Game.Navigator;
 
 public class Environment
 {
@@ -11,6 +12,7 @@ public class Environment
     private MessageHandler m_message_handler = null;
     private DatabaseManager m_database_manager = null;
     private SessionManager m_session_manager = null;
+    private NavigatorManager m_navigator_manager = null;
     public MessageHandler return_message_handler()
     {
         return m_message_handler;
@@ -26,6 +28,11 @@ public class Environment
         return m_session_manager;
     }
 
+    public NavigatorManager return_navigator_manager()
+    {
+        return m_navigator_manager;
+    }
+
     public Environment()
     {
        Logging.m_Logger.Info("A server environment has been intialized successfully.");
@@ -35,5 +42,9 @@ public class Environment
         m_message_handler = new MessageHandler();
         m_database_manager = new DatabaseManager();
         m_session_manager = new SessionManager();
+        m_navigator_manager = new NavigatorManager();
+
+
+        m_navigator_manager.LoadNavigator();
     }
 }
--- Emulator/Game/Models/NavigatorCategory.cs
+++ Emulator/Game/Models/NavigatorCategory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Emulator.Game.Database;
+using NHibernate;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +18,53 @@ namespace Emulator.Game.Models
 
         public virtual int category_parent_id { get; set; }
 
+        public virtual IList<NavigatorCategory> return_sub_categories()
+        {
+            IList<NavigatorCategory> sub_categories_list;
+
+            using (ISession m_session = DatabaseManager.openSession())
+            {
+                sub_categories_list = m_session.QueryOver<NavigatorCategory>().Where(x => x.category_parent_id == category_id).List();
+                m_session.Close();
+            }
+
+            return sub_categories_list;
+        }
+
+        public virtual int return_room_count()
+        {
+            int m_count;
+
+            using (ISession m_session = DatabaseManager.openSession())
+            {
+                m_count = m_session.QueryOver<NavigatorPrivates>().Where(x => x.room_category_id == category_id).RowCount();
+                m_session.Close();
+            }
+
+            return m_count;
+
+
+        }
+
+        public virtual IList return_child_rooms()
+        {
+            IList m_room_list;
+
+            using (ISession m_session = DatabaseManager.openSession())
+            {
+                if (category_type == 0)
+                {
+                    m_room_list = (IList)m_session.QueryOver<NavigatorPublics>().Where(x => x.room_category_id == category_id).List();
+                }
+                else
+                {
+                    m_room_list = (IList)m_session.QueryOver<NavigatorPrivates>().Where(x => x.room_category_id == category_id).List();
+                }
+
+                m_session.Close();
+            }
+
+            return m_room_list;
+        }
     }
 }
